02afaf3f9285a6ee909aae3440938bc978f387df
HapiFive <1143910315@qq.com>
2024-04-17 05:56:02 +0800

    Revert "一些更正"
    This reverts commit a8eb8cb69c12c91109e1e871b34d2d6a2510c7ad.

--- mesonproject.cpp
+++ mesonproject.cpp
@@ -17,44 +17,46 @@
 using namespace ProjectExplorer;
 
 namespace MesonProjectManager {
-    namespace Internal {
-        MesonProject::MesonProject(const Utils::FilePath &path)
-            : Project{Constants::Project::MIMETYPE, path} {
-            setId(Constants::Project::ID);
-            setProjectLanguages(Core::Context(ProjectExplorer::Constants::CXX_LANGUAGE_ID));
-            setDisplayName(projectDirectory().fileName());
-            setCanBuildProducts();
-            setHasMakeInstallEquivalent(true);
-        }
+namespace Internal {
 
-        Tasks MesonProject::projectIssues(const Kit *k) const {
-            Tasks result = Project::projectIssues(k);
+MesonProject::MesonProject(const Utils::FilePath &path)
+    : Project{Constants::Project::MIMETYPE, path}
+{
+    setId(Constants::Project::ID);
+    setProjectLanguages(Core::Context(ProjectExplorer::Constants::CXX_LANGUAGE_ID));
+    setDisplayName(projectDirectory().fileName());
+    setCanBuildProducts();
+    setHasMakeInstallEquivalent(true);
+}
 
-            if (!MesonToolKitAspect::isValid(k)) {
-                result.append(
-                    createProjectTask(Task::TaskType::Error, Tr::tr("No Meson tool set.")));
-            }
-            if (!NinjaToolKitAspect::isValid(k)) {
-                result.append(
-                    createProjectTask(Task::TaskType::Error, Tr::tr("No Ninja tool set.")));
-            }
-            if (ToolchainKitAspect::toolChains(k).isEmpty()) {
-                result.append(createProjectTask(Task::TaskType::Warning,
-                                                Tr::tr("No compilers set in kit.")));
-            }
-            return result;
-        }
+Tasks MesonProject::projectIssues(const Kit *k) const
+{
+    Tasks result = Project::projectIssues(k);
 
-        ProjectImporter *MesonProject::projectImporter() const {
-            if (m_projectImporter) {
-                m_projectImporter = std::make_unique<MesonProjectImporter>(projectFilePath());
-            }
-            return m_projectImporter.get();
-        }
+    if (!MesonToolKitAspect::isValid(k))
+        result.append(
+            createProjectTask(Task::TaskType::Error, Tr::tr("No Meson tool set.")));
+    if (!NinjaToolKitAspect::isValid(k))
+        result.append(
+            createProjectTask(Task::TaskType::Error, Tr::tr("No Ninja tool set.")));
+    if (ToolChainKitAspect::toolChains(k).isEmpty())
+        result.append(createProjectTask(Task::TaskType::Warning,
+                                        Tr::tr("No compilers set in kit.")));
+    return result;
+}
 
-        DeploymentKnowledge MesonProject::deploymentKnowledge() const {
-            // TODO in next releases
-            return DeploymentKnowledge::Bad;
-        }
-    } // namespace Internal
+ProjectImporter *MesonProject::projectImporter() const
+{
+    if (m_projectImporter)
+        m_projectImporter = std::make_unique<MesonProjectImporter>(projectFilePath());
+    return m_projectImporter.get();
+}
+
+DeploymentKnowledge MesonProject::deploymentKnowledge() const
+{
+    // TODO in next releases
+    return DeploymentKnowledge::Bad;
+}
+
+} // namespace Internal
 } // namespace MesonProjectManager
